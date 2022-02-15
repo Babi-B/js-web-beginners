23ccae86ac4fe53b28e608008b27673c850f01ec
Babi-B <babibyoolah@gmail.com>
2022-02-15 11:27:24 +0100

    refactoring

--- password-generator/app.js
+++ password-generator/app.js
@@ -7,46 +7,44 @@ let gen = document.getElementById("gen");
 let copy = document.getElementById("copy");
 let screen = document.getElementById("screen");
 
-
 gen.addEventListener("click", getPassword);
 copy.addEventListener("click", copyToClipboard);
 
-
 function getPassword() {
-    screen.value = screen.innerHTML = generatePassword();
+  screen.value = screen.innerHTML = generatePassword();
 }
-function copyToClipboard(){
-    navigator.clipboard.writeText(screen.value);
+function copyToClipboard() {
+  navigator.clipboard.writeText(screen.value);
 }
 
-    /* To Generate the password*/
+/* To Generate the password*/
 function generatePassword() {
-    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let symbols = "%!@#$^&*-+=|\\(){}:\"';,?";
-    let password = "";
-  
-    for (let i = 0; i < 4; i++) {
-      let randomNumber = Math.floor(Math.random() * 10);
-      let lowerCaseLetter = alphabet.charAt(Math.random() * 26).toLowerCase();
-      let upperCaseLetter = alphabet.charAt(Math.random() * 26).toUpperCase();
-      let specialChar = symbols.charAt(Math.random() * symbols.length);
-  
-      password += randomNumber + lowerCaseLetter + upperCaseLetter + specialChar;
-    }
-    return shuffle(password);
+  let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let symbols = "%!@#$^&*-+=|\\(){}:\"';,?";
+  let password = "";
+
+  for (let i = 0; i < 4; i++) {
+    let randomNumber = Math.floor(Math.random() * 10);
+    let lowerCaseLetter = alphabet.charAt(Math.random() * 26).toLowerCase();
+    let upperCaseLetter = alphabet.charAt(Math.random() * 26).toUpperCase();
+    let specialChar = symbols.charAt(Math.random() * symbols.length);
+
+    password += randomNumber + lowerCaseLetter + upperCaseLetter + specialChar;
   }
-  
-  /* To shuffle the password string*/
-  function shuffle(str) {
-    let arr = str.split("");
-    let n = arr.length;
-  
-    for (let i = 0; i < n; i++) {
-      let j = Math.floor(Math.random() * n);
-  
-      let temp = arr[i];
-      arr[i] = arr[j];
-      arr[j] = temp;
-    }
-    return arr.join("");
+  return shuffle(password);
+}
+
+/* To shuffle the password string*/
+function shuffle(str) {
+  let arr = str.split("");
+  let n = arr.length;
+
+  for (let i = 0; i < n; i++) {
+    let j = Math.floor(Math.random() * n);
+
+    let temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
   }
+  return arr.join("");
+}
